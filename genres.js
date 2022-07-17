71832a537b81f392ef3cae815bc6d94ec4de7997
Wong Elone <elone0908@gmail.com>
2022-07-17 15:14:33 +0800

    v2.0.0 Implement MongoDB to store genres.

--- genres.js
+++ genres.js
@@ -1,51 +1,66 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 
+const Genre = mongoose.model('Genre', mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    }
+}));
+
+const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
-let lastID = 2;
 
-const genres = [
-    { id: 1, name: 'thriller' },
-    { id: 2, name: 'romance' },
-]
+// const genres = [
+//     { id: 1, name: 'thriller' },
+//     { id: 2, name: 'romance' },
+// ]
 
-router.get('/', (req, res) => res.send(genres));
+router.get('/', async (req, res) => {
+    const genres = await Genre
+        .find()
+        .sort('name');
+    res.send(genres);
+});
 
-router.get('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
+
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
     res.send(genre);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = {
-        id: (lastID ++) + 1,
-        name: req.body.name
-    };
-    genres.push(genre);
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
     res.send(genre);
 });
 
-router.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
+router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+    
+    const genre = await Genre.findByIdAndUpdate(
+        req.params.id, 
+        { name: req.body.name }, 
+        { new: true });
 
-    genre.name = req.body.name;
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
     res.send(genre);
 });
 
-router.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
-    genres.splice(genres.indexOf(genre), 1);
     res.send(genre);
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const Joi = require('joi');
 const genres = require('./genres');
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(() => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
