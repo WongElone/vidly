274347451e75dfaa890bd492c545d45eba0f638b
Wong Elone <elone0908@gmail.com>
2022-07-31 18:57:24 +0800

    Setup production settings for heroku.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/db')();
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
